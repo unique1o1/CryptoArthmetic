fd1edab39a4f4eb18ceb4f4c103c654407435c69
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-06-07 22:24:12 +0545

    comments added

--- main.py
+++ main.py
@@ -4,20 +4,22 @@ import sys
 
 
 def word_to_number(word, lookup):
-    """Convert a word to a number by replacing each letter with a digit, using a lookup dictionary."""
+    """Convert a word to a number by replacing each letter with a digit"""
     return int(''.join(str(lookup[letter]) for letter in word))
 
 
 def solve_problem(words):
     words = [word.upper() for word in words]
-    letters = list(set(''.join(words)))
+    letters = list(set(''.join(words))) # Remove duplicate letters and create a list
     if len(letters) > 10:
         print('Too many distinct letters ({})'.format(len(letters)))
         exit(0)
 
     for perm in permutations(range(10), len(letters)):
+        # Create a hash value for key value pair i.e letter is key number is value
         lookup = dict(zip(letters, perm))
         if all(lookup[i[0]] > 0 for i in words):
+            # array of words converted into numbers i.e [123,123,432]
             numbers = [word_to_number(w, lookup) for w in words]
 
             if sum(numbers[:-1]) == numbers[-1]:
