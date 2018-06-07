26d9022980fae64203b5dc1e39b3c2f22ba77ffe
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-06-07 22:07:44 +0545

    first step

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 
 from itertools import permutations
 import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print('At least three arguments required.')
+    else:
+        for answer in solve.problem(sys.argv[1:]):
+            print("{}+{}={}".format(*answer))
