abfa5806f4722e42c59cb6d26abb4494588e55f3
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-06-14 17:07:30 +0545

    minor change

---
+++ cy_thon.pyx
@@ -0,0 +1,35 @@
+
+from itertools import permutations
+import sys
+
+
+def solve_problem(words):
+    # Remove duplicate letters and create a list
+    letters = list(set(''.join(words)))
+    if len(letters) > 10:
+        print('Too many distinct letters ({})'.format(len(letters)))
+        exit(0)
+
+    for perm in permutations(range(10), len(letters)):
+        # Create a hash value for key value pair i.e letter is key number is value
+        lookup = dict(zip(letters, perm))
+        if all(lookup[i[0]] > 0 for i in words):
+            # array of words converted into numbers i.e [123,123,432]
+            numbers = [word_to_number(w, lookup) for w in words]
+
+            if sum(numbers[:-1]) == numbers[-1]:
+                yield numbers
+
+
+def word_to_number(word, lookup):
+    """Convert a word to a number by replacing each letter with a digit"""
+    return int(''.join(str(lookup[letter]) for letter in word))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4:
+        print('At least three arguments are required.')
+    else:
+        for answer in solve_problem(sys.argv[1:]):
+            print(' + '.join(str(n)
+                             for n in answer[:-1]) + ' = ' + str(answer[-1]))
