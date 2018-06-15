cee2f08c7db61110afef5f071edabf5bcb672a15
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-06-15 14:21:50 +0545

    cython used

--- cy_thon.pyx
+++ cy_thon.pyx
@@ -3,16 +3,22 @@ from itertools import permutations
 import sys
 
 
-def solve_problem(words):
+def  solve_problem(list words):
     # Remove duplicate letters and create a list
+    cdef list letters
+   
     letters = list(set(''.join(words)))
     if len(letters) > 10:
         print('Too many distinct letters ({})'.format(len(letters)))
         exit(0)
-
+    cdef tuple perm
+    cdef char i[10]
+    cdef dict lookup
     for perm in permutations(range(10), len(letters)):
         # Create a hash value for key value pair i.e letter is key number is value
-        lookup = dict(zip(letters, perm))
+
+        lookup=dict(zip(letters, perm))
+        
         if all(lookup[i[0]] > 0 for i in words):
             # array of words converted into numbers i.e [123,123,432]
             numbers = [word_to_number(w, lookup) for w in words]
@@ -21,8 +27,7 @@ def solve_problem(words):
                 yield numbers
 
 
-def word_to_number(word, lookup):
+cdef int word_to_number( word, lookup):
     """Convert a word to a number by replacing each letter with a digit"""
+    cdef char letter
     return int(''.join(str(lookup[letter]) for letter in word))
-
-
