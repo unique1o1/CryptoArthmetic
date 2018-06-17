f955deaab75011eab3c74d8d92b54139e14e3020
Yunik Maharjan <yunik.maharjan@icloud.com>
2018-06-17 15:49:54 +0545

    optimization changes -notworking

--- cy_thon.pyx
+++ cy_thon.pyx
@@ -6,34 +6,42 @@ import sys
 def  solve_problem(list words):
     # Remove duplicate letters and create a list
     cdef:
+        tuple perm
+     
+        dict lookup
         list letters
-        list numbers
-    letters = list(set(''.join(words)))
+
+    unique_words=list(set(words))
+    letters = list(set(''.join(unique_words)))
+    
     if len(letters) > 10:
         print('Too many distinct letters ({})'.format(len(letters)))
         exit(0)
-    yield from permu(letters,words)
 
-def permu(list letters,list words):
-    cdef:
-        tuple perm
-        list numbers
-        dict lookup
+    
     for perm in permutations(range(10), len(letters)):
         # Create a hash value for key value pair i.e letter is key number is value
 
         lookup=dict(zip(letters, perm))
  
-        if all(lookup[i[0]] > 0 for i in words):
+        if all(lookup[i[0]] > 0 for i in unique_words):
             # array of words converted into numbers i.e [123,123,432]
             
-            numbers = [int(''.join(str(lookup[letter]) for letter in w)) for w in words]
-
+            for w in unique_words:
+                numbers.extend(word_to_number(w,lookup,words))
             if sum(numbers[:-1]) == numbers[-1]:
                 yield numbers
 
 
-# def word_to_number(str word, dict lookup):
-#     """Convert a word to a number by replacing each letter with a digit"""
-#     cdef char letter
-#     return 
+def word_to_number(str word, dict lookup,list words):
+    """Convert a word to a number by replacing each letter with a digit"""
+    cdef :
+        
+        int size=len(word)
+        int it=0
+    for i,w in enumerate(word,1):
+        it+=lookup[w]*(10**(size-i))
+    cdef int c=words.count(word)
+    if c>1:
+        return [it]*c
+    return [it]
